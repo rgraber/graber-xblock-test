63debedda6a1775477a385c34156b9ffc9ac1999
Rebecca Graber <rgraber@2u.com>
2023-12-14 16:04:00 -0500

    fix: sure

---
+++ tests/test_my_xblock.py
@@ -0,0 +1,21 @@
+"""
+Tests for pipeline.py
+"""
+from unittest.mock import Mock
+
+from django.test import TestCase
+from my_xblock import MyXBlock
+from xblock.test.toy_runtime import ToyRuntime
+from xblock.fields import ScopeIds
+
+class TestMyXBlock(TestCase):
+    def setUp(self) -> None:
+        self.maxDiff = None
+
+    def test_my_student_view(self):
+        scope_ids = ScopeIds('1','2','3','4')
+        block = MyXBlock(ToyRuntime(), scope_ids=scope_ids)
+        frag = block.student_view()
+        as_dict = frag.to_dict()
+        content = as_dict['content']
+        self.assertIn('MyXBlock: count is now', content, 'XBlock did not render correct student view')
